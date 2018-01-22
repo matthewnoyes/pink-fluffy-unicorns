238c508b191596901cafdc06383f6d83ebd1f3f7
Matt <matthew@graoil.co.uk>
2018-01-22 22:09:51 +0000

    Added methods for current stock retrevial

--- virtualassistant/data/stocks/IStockData.java
+++ virtualassistant/data/stocks/IStockData.java
@@ -6,8 +6,14 @@ public interface IStockData {
   public String getTicker(String company);
 
   public String getSector(String ticker);
-  public String getTickersInSector(String sector);
+  public String[] getTickersInSector(String sector);
 
   public double getCurrentPrice(String ticker);
+  public double getChange(String ticker);
+  public double getPercentageChange(String ticker);
+
+  public double getCurrentSectorPrice(String sector);
+  public double getSectorChange(String sector);
+  public double getSectorPercentageChange(String sector);
 
 }
