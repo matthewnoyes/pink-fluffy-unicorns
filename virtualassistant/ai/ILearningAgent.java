93d9ca39b187095e84a9d5aefe182b8ce4078182
blodyelf <31694590+blodyelf@users.noreply.github.com>
2018-01-23 11:01:48 +0000

    Update ILearningAgent.java

--- virtualassistant/ai/ILearningAgent.java
+++ virtualassistant/ai/ILearningAgent.java
@@ -2,5 +2,13 @@ package virtualassistant.ai;
 
 //The AI section
 public interface ILearningAgent {
-
+  
+  // Take tokenized string and patternized string and compare one against the other
+  public void analyzeInput(List<String> tokenized, List<String> patternized);
+  
+  // 
+  public void getFavourites();
+  
+  // Constantly search for anomalies
+  public static void main(String[] args);
 }
