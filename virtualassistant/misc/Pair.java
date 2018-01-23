a19da32ec2b811995abf9af16887d532452adab2
blodyelf <31694590+blodyelf@users.noreply.github.com>
2018-01-23 10:48:25 +0000

    Update Pair.java

--- virtualassistant/misc/Pair.java
+++ virtualassistant/misc/Pair.java
@@ -1,3 +1,30 @@
 package virtualassistant.misc;
 
-public class Pair {
+public class Pair<K, V> {
+  
+  private K first;
+  private V second;
+  
+  public Pair(K first, V second) {
+    this.first = first;
+    this.second = second;
+  }
+  
+  // Getters
+  public K getFirst(){
+    return first;
+  }
+  
+  public V getSecond(){
+    return second;
+  }
+  
+  // Setters
+  public void setFirst(K first) {
+    this.first = first;
+  }
+  
+  public void setSecond(V second) {
+    this.second = second;
+  }
+}
