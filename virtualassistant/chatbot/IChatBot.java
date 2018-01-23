9ae9351355b42d48f3fda2ddc259d15be66ba747
blodyelf <31694590+blodyelf@users.noreply.github.com>
2018-01-23 10:55:27 +0000

    Update IChatBot.java

--- virtualassistant/chatbot/IChatBot.java
+++ virtualassistant/chatbot/IChatBot.java
@@ -1,6 +1,9 @@
 package virtualassistant.chatbot;
+import  virtualassistant.misc.*;
 
 //The NLP
 public interface IChatBot {
-
+  
+  // return action and parsed string as tokens
+  public Pair<Integer, List<String>> parse(String s);
 }
