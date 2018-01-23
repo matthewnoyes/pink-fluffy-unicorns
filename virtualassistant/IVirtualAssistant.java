036c1ec1804c600ac587da82fd36b11becd3379e
blodyelf <31694590+blodyelf@users.noreply.github.com>
2018-01-23 10:32:44 +0000

    Update IVirtualAssistant.java

--- virtualassistant/IVirtualAssistant.java
+++ virtualassistant/IVirtualAssistant.java
@@ -2,4 +2,15 @@ package virtualassistant;
 
 public interface IVirtualAssistant {
   
+  public static void main(String args[]);
+  
+  // Startup & update memory
+  public static void load();
+  public static void updateMemory();
+  
+  // I/O
+  public static Pair<String, List<String, Int>> processInput();
+  
+  //Actions
+  public static String action1();
 }
