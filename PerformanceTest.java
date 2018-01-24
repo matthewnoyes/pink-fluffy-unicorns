4153e37128919d0ba0d5e87765c52e7ee56c93ec
blodyelf <31694590+blodyelf@users.noreply.github.com>
2018-01-24 14:18:27 +0000

    Create PerformanceTest.java

---
+++ PerformanceTest.java
@@ -0,0 +1,39 @@
+package virtualassistant.tests;
+
+// IMPORTANT: Only use after ProgramTest returns SUCCESS 
+public class PerformanceTest {
+  
+  public static void main(String[] args) {
+    
+    long start, end;
+    
+    // Test instantiations
+    System.out.println("Instantiation testing (nanoseconds)...");
+    System.out.print("Instantiating Dictionary... ");
+    
+    start = System.nanoTime();
+    Dictionary dictionary = new Dictionary();
+    end = System.nanoTime();
+    
+    System.out.println((end - start) + "ns");
+     
+    System.out.print("Instantiating NPLParser... ");
+    
+    start = System.nanoTime();
+    NPLParser npl = new NPLParser();
+    end = System.nanoTime();
+    
+    System.out.println((end - start) + "ns");
+    
+    System.out.print("Instantiating AI... ");
+    
+    start = System.nanoTime();
+    AI ai = new AI();
+    end = System.nanoTime();
+    
+    System.out.println((end - start) + "ns");
+    
+    // Test methods
+    
+  }
+}
