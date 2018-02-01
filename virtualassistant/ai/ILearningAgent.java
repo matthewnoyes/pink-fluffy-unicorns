cc6f39a12271246d4406f472bd6b876b96c10a28
Matt <matthew@graoil.co.uk>
2018-02-01 20:06:13 +0000

    Updated the AI interface

--- virtualassistant/ai/ILearningAgent.java
+++ virtualassistant/ai/ILearningAgent.java
@@ -2,13 +2,21 @@ package virtualassistant.ai;
 
 //The AI section
 public interface ILearningAgent {
-  
+
   // Take tokenized string and patternized string and compare one against the other
   public void analyzeInput(List<String> tokenized, List<String> patternized);
-  
-  // 
-  public void getFavourites();
-  
-  // Constantly search for anomalies
-  public static void main(String[] args);
+
+  //?
+  public String[] getFavourites();
+  //Lets the AI know the user has bookmarked the stock
+  public void bookmarkStock(String ticker);
+
+  public String[] suggestQueries(int count);
+
+  //For notifications (when the data gets updated):
+
+  //Constantly search for stock anomalies
+  public void searchForStockEvent();
+  //Constantly search for news anomalies
+  public void searchForNewsEvent();
 }
