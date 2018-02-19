112daf43363ab6e188448f953cced2a2ee4cd232
Daniel Smith <dannydjrs@users.noreply.github.com>
2018-02-19 20:53:59 +0000

    Update Message.java
    Add needed code for Message.java

--- virtualassistant/gui/Message.java
+++ virtualassistant/gui/Message.java
@@ -1,1 +1,17 @@
+public class Message {
 
+protected String message;
+
+public Message(String message) {
+				this.message = message;
+}
+
+public String getMessage() {
+				return message;
+}
+
+public News[] getNews() {
+				return null;
+}
+
+}
