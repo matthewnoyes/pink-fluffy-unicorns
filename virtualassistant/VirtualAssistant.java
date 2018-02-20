23c01368dea0f49f2aae6f690cc2916364091a7f
Matt <matthew@graoil.co.uk>
2018-02-20 12:09:24 +0000

    Changed compling script

--- virtualassistant/VirtualAssistant.java
+++ virtualassistant/VirtualAssistant.java
@@ -1,90 +1,92 @@
 package virtualassistant;
 
 public class VirtualAssistant {
-    
+
     private StockData stockdata;
     private Loader loader;
     private LearningAgent learningAgent;
     private NewsProcessor newsProcessor;
     private Chatbot chatbot;
-    
+
     public static void main(String[] args) {
-        
+
         //Instntiate everything
-        
+
         loader = new Loader();
         stockData = loader.readStocks();
         learningAgent = loader.readLearningAgent();
         newsProcessor = new NewsProcessor();
         chatbot = new Chatbot();
-        
+
         //Try to update data.
-        
+
         for(;;){
             //Wait a bit
             TimeUnit.SECONDS.sleep(10);
-            
+
             // Try to update data, if working, fire learning agent
             if(loader.updateData(stockData)) {
-                
+
                 learningAgent.searchForStockEvent();
                 learningAgent.searchForNewsEvent();
             }
-            
+
         }
     }
-    
+
     // Decide action type based on action type decided by chatbot?
     public static void decideAction(Action actionType, List<String> parameters){
-        
+
         switch(actionType){
-            
+
             case Action.GET_COMPANY_DATA:  getCompanyData(parameters);
                                     break;
-                
+
             case Action.GET_SECTOR_DATA:   getSectorData(parameters);
                                     break;
-                
+
             case Action.COMPARE_COMPANIES: compareCompanies(parameters);
                                     break;
-                
+
             case Action.COMPARE_SECTORS:   compareSectors(parameters);
                                     break;
-                
+
             case Action.ALERT:             alert(parameters);
                                     break;
-            
+
             default:                System.out.println("Undefined action!");
                                     break;
         }
-        
-        
+
+
     }
-    
+
     /* Example action
     *
     *   For this action, the first parameter should always be the company name, followed by
     *   type of data requested.
     */
-    
+
     public static void getCompanyData(List<String> parameters){
-        
+
         Company company = stockData.companyForName(parameters.remove(0));
-        
+
         for(String str : parameters) {
-            
+
             switch(str) {
-                
+
                 case "open":
                     chatbot.output(company.open);
                     break;
-                
-                case ...
+
+                case "close":
+                  chatbot.output(company.close);
+                  break;
             }
         }
     }
-    
-    
+
+
 }
 
 public enum Action {
--- virtualassistant/virtualassistant.sh
+++ virtualassistant/virtualassistant.sh
@@ -1,1 +1,7 @@
 #Compile all java files
+javac ./ai/*.java chatbot/*.java data/datastore/*.java data/news/*.java data/stocks/*.java gui/*.java misc/*.java IVirtualAssistant.java VirtualAssistant.java -cp virtualassistant/data/jsoup-1.11.2.jar || { echo 'Compiling Failed' ; exit 1; }
+
+jar cfe virtualassistant.jar virtualassistant.VirtualAssistant * || { echo 'Creating jar file failed' ; exit 1; }
+
+echo 'Succesfully created jar file';
+echo 'Run "java -jar virtualassistant.jar" to run program';
