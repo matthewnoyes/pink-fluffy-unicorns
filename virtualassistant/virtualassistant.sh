90cf5509747d343492da64be9815e38b0480a74e
Matt <matthew@graoil.co.uk>
2018-02-21 10:45:56 +0000

    Changed compiling

--- virtualassistant/virtualassistant.sh
+++ virtualassistant/virtualassistant.sh
@@ -1,7 +1,11 @@
+cd "$(dirname "$0")"
+
+classpath='.:./data/jsoup-1.11.2.jar:./data/datastore/JSON-parser.jar';
+
 #Compile all java files
-javac ./ai/*.java chatbot/*.java data/datastore/*.java data/news/*.java data/stocks/*.java gui/*.java misc/*.java IVirtualAssistant.java VirtualAssistant.java -cp virtualassistant/data/jsoup-1.11.2.jar || { echo 'Compiling Failed' ; exit 1; }
+javac -cp $classpath ./ai/*.java chatbot/*.java data/news/*.java data/stocks/*.java data/datastore/*.java gui/*.java misc/*.java IVirtualAssistant.java VirtualAssistant.java || { echo 'Compiling Failed' ; exit 1; }
 
 jar cfe virtualassistant.jar virtualassistant.VirtualAssistant * || { echo 'Creating jar file failed' ; exit 1; }
 
 echo 'Succesfully created jar file';
-echo 'Run "java -jar virtualassistant.jar" to run program';
+#echo 'Run "java -jar virtualassistant.jar" to run program';
