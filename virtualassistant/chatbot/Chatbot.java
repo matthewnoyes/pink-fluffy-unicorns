fb67fdea488eaac1a71f6482506d93bade1841ff
luwaa <31140854+luwaa@users.noreply.github.com>
2018-02-24 15:11:10 +0000

    Update Chatbot.java
    Removed imports not needed.

--- virtualassistant/chatbot/Chatbot.java
+++ virtualassistant/chatbot/Chatbot.java
@@ -1,8 +1,5 @@
 import java.io.IOException;  
 import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 import org.json.simple.parser.ParseException;
 import org.json.simple.parser.JSONParser;
