e1e8305c92e71e534c793745550a82cebfe37338
Daniel Smith <dannydjrs@users.noreply.github.com>
2018-02-25 11:49:50 +0000

    Create Main.java
    Main class

---
+++ virtualassistant/gui/Main.java
@@ -0,0 +1,41 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+public static float WIDTH = 500;
+public static float HEIGHT = 700;
+
+public static void main(String[] args) {
+		launch(args);
+}
+
+@Override
+public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("Trader's Assistant");
+		primaryStage.setResizable(false);
+		primaryStage.setOnCloseRequest(e->{
+						e.consume();
+						closeProgram();
+				});
+
+		Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+		Scene scene = new Scene(root, WIDTH, HEIGHT);
+		scene.getStylesheets().add("sample/styles/stylesheet.css");
+
+		primaryStage.setScene(scene);
+		primaryStage.show();
+
+}
+
+public void closeProgram() {
+
+}
+
+
+}
